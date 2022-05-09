affb03dd85b545a5e8252fc67ad96ee720188999
skokhatska <fCfjMpSKmF7w>
2022-05-09 16:29:24 +0400

    assignment recipes on index page

---
+++ src/main/java/guru/springframework/bootstrap/RecipesBootstrap.java
@@ -0,0 +1,155 @@
+package guru.springframework.bootstrap;
+
+import guru.springframework.domain.*;
+import guru.springframework.repositories.CategoryRepository;
+import guru.springframework.repositories.RecipeRepository;
+import guru.springframework.repositories.UnitOfMeasureRepository;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class RecipesBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+    private RecipeRepository recipeRepository;
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public RecipesBootstrap(RecipeRepository recipeRepository, CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.recipeRepository = recipeRepository;
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        recipeRepository.saveAll(getRecipes());
+    }
+
+    private List<Recipe> getRecipes() {
+        List<Recipe> recipes = new ArrayList<>(2);
+
+        Optional<UnitOfMeasure> eachUomOptional = unitOfMeasureRepository.findByDescription("Each");
+
+        if (!eachUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found!");
+        }
+
+        Optional<UnitOfMeasure> tablespoonUomOptional = unitOfMeasureRepository.findByDescription("Tablespoon");
+
+        if (!tablespoonUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found!");
+        }
+
+        Optional<UnitOfMeasure> teaspoonUomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        if (!teaspoonUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found!");
+        }
+
+        Optional<UnitOfMeasure> dashUomOptional = unitOfMeasureRepository.findByDescription("Dash");
+
+        if (!dashUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found!");
+        }
+
+        Optional<UnitOfMeasure> pintUomOptional = unitOfMeasureRepository.findByDescription("Pint");
+
+        if (!pintUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found!");
+        }
+
+        Optional<UnitOfMeasure> cupUomOptional = unitOfMeasureRepository.findByDescription("Cup");
+
+        if (!cupUomOptional.isPresent()) {
+            throw new RuntimeException("Expected UOM Not Found!");
+        }
+
+        //get optionals
+        UnitOfMeasure eachUom = eachUomOptional.get();
+        UnitOfMeasure tablespoonUom = tablespoonUomOptional.get();
+        UnitOfMeasure teaspoonUom = teaspoonUomOptional.get();
+        UnitOfMeasure dashUom = dashUomOptional.get();
+        UnitOfMeasure pintUom = pintUomOptional.get();
+        UnitOfMeasure cupUom = cupUomOptional.get();
+
+        //get Categories
+        Optional<Category> americanCategoryOptional = categoryRepository.findByDescription("American");
+
+        if (!americanCategoryOptional.isPresent()) {
+            throw new RuntimeException("Expected Category Not Found!");
+        }
+
+        Optional<Category> mexicanCategoryOptional = categoryRepository.findByDescription("Mexican");
+
+        if (!mexicanCategoryOptional.isPresent()) {
+            throw new RuntimeException("Expected Category Not Found!");
+        }
+
+        Category americanCategory = americanCategoryOptional.get();
+        Category mexicanCategory = mexicanCategoryOptional.get();
+
+
+        // Guacamole
+        Recipe guacRecipe = new Recipe();
+        guacRecipe.setDescription("Perfect Guacamole");
+        guacRecipe.setPrepTime(10);
+        guacRecipe.setCookTime(0);
+        guacRecipe.setDifficulty(Difficulty.EASY);
+        guacRecipe.setDirections("A lot of steps");
+
+        Notes guacNotes = new Notes(guacRecipe);
+        guacNotes.setRecipeNotes("Be careful handling chilis! If using, it's best to wear food-safe gloves. \n" +
+                "If no gloves are available, wash your hands thoroughly after handling, and do not touch your eyes or \n" +
+                "the area near your eyes for several hours afterwards.");
+
+        guacRecipe.setNotes(guacNotes);
+        guacRecipe.getIngredients().add(new Ingredient("ripe avocados", new BigDecimal(2), eachUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("Kosher salt", new BigDecimal(".5"), teaspoonUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("fresh lime or lemon juice", new BigDecimal(1), tablespoonUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("minced red onion or thinly sliced green onion", new BigDecimal(2), tablespoonUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("serrano (or jalapeño) chilis, stems and seeds removed, minced", new BigDecimal(1), eachUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("cilantro (leaves and tender stems), finely chopped", new BigDecimal(2), tablespoonUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("freshly ground black pepper", new BigDecimal(1), pintUom, guacRecipe));
+        guacRecipe.getIngredients().add(new Ingredient("ripe tomato, chopped (optional)", new BigDecimal(".5"), eachUom, guacRecipe));
+
+        guacRecipe.getCategories().add(americanCategory);
+        guacRecipe.getCategories().add(mexicanCategory);
+        recipes.add(guacRecipe);
+
+        // Tacos
+        Recipe tacosRecipe = new Recipe();
+        tacosRecipe.setDescription("Spicy Grilled Chicken Tacos");
+        tacosRecipe.setPrepTime(20);
+        tacosRecipe.setCookTime(15);
+        tacosRecipe.setDifficulty(Difficulty.MODERATE);
+        tacosRecipe.setDirections("A lot of steps");
+
+        Notes tacosNotes = new Notes(tacosRecipe);
+        tacosNotes.setRecipeNotes("Look for ancho chile powder with the Mexican ingredients at your grocery store, \n" +
+                "on buy it online. (If you can't find ancho chili powder, you replace the ancho chili, the oregano, \n" +
+                "and the cumin with 2 1/2 tablespoons regular chili powder, though the flavor won't be quite the same.");
+
+        tacosRecipe.setNotes(tacosNotes);
+        tacosRecipe.getIngredients().add(new Ingredient("ancho chili powder", new BigDecimal(2), tablespoonUom, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("dried oregano", new BigDecimal(1), teaspoonUom, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("dried cumin", new BigDecimal(1), teaspoonUom, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("sugar", new BigDecimal(1), teaspoonUom, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("salt", new BigDecimal(".5"), teaspoonUom, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("clove garlic, finely chopped", new BigDecimal(1), eachUom, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("finely grated orange zest", new BigDecimal(1), tablespoonUom, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("fresh-squeezed orange juice", new BigDecimal(3), tablespoonUom, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("olive oil", new BigDecimal(2), tablespoonUom, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("skinless, boneless chicken thighs", new BigDecimal(6), eachUom, tacosRecipe));
+
+        tacosRecipe.getCategories().add(americanCategory);
+        tacosRecipe.getCategories().add(mexicanCategory);
+        recipes.add(tacosRecipe);
+
+        return recipes;
+    }
+}
--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -14,6 +14,13 @@ public class Notes {
     @Lob
     private String recipeNotes;
 
+    public Notes() {
+    }
+
+    public Notes(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
     public Long getId() {
         return id;
     }
